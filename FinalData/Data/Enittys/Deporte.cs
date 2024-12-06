4c4d5c22c79e1778bbf173a5ad3a10ff6770a9a9
K0mir <vfm4003675@est.univalle.edu>
2024-12-06 09:04:14 -0400

    Entidades

---
+++ FinalData/Data/Enittys/Deporte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Final.Data.Data.Entitys
+{
+    [Table("Deporte")]
+    public class Deporte
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DeporteId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int CantJugadores { get; set; }
+        public DateTime FechaCracion { get; set; }
+        public int Popularidad { get; set; }
+
+        public int IdTipo { get; set; }
+        [ForeignKey("IdTipo")]
+        public TipoDeporte? TipoDeporte { get; set; }
+    }
+}
---
+++ FinalData/Data/Enittys/TipoDeporte.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Final.Data.Data.Entitys;
+namespace Final.Data.Data.Entitys
+{
+    [Table("TipoDeporte")]
+    public class TipoDeporte
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdTipo { get; set; }
+        public string NombreTipo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+        public virtual List<Deporte>? Deportes { get; set; }
+    }
+}
